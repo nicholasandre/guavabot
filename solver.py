9cbff1534837459516aaa026eac5ba7fbf04b46c
Nicholas Andre <nicholasandre@berkeley.edu>
2019-05-01 23:16:51 -0700

    step 1 nearly done

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 # Put your solution here.
 
+import networkx as nx
 from math import ceil
 
 def solve(client):
@@ -14,24 +15,42 @@ def solve(client):
     bots_found = 0
     students = list(range(1, num_students + 1))
 
+    #The dictionary of how many bots we currently know at each vertex
+    bots_at = {}
+
     #The dictionary of unvisited nodes being the key, with the score of the node being the value
-    unvisited = list(graph.nodes())
-    
+    unvisited = {}
+    unvisited_list = list(graph.nodes())
+    unvisited_list.remove(home)
+    bots_at[home] = 0
+    for i in unvisited_list:
+    	unvisited[i] = 0
+    	bots_at[i] = 0
     lies = {}
+
+    #The dictionary of vertices being the key and dictionary of scout result of that vertex being the value
     scout_result = {}
     mst = nx.minimum_spanning_tree(graph)
-    unvisited.remove(home)
     bots_locations = []
     for k in students:
     	lies[k] = 0
     	probability[k] = 0.5
     for i in unvisited:
-    	result = client.scout(i, students)
-    	scout_result[i] = result
+    	scout_result[i] = client.scout(i, students)
 
     while bots_found < num_bots:
     	for vertex in unvisited:
-    		count_score(vertex)
+    		unvisited[vertex] = count_score(vertex, scout_result, lies)
+    	most_probable_vertex = max(unvisited, key=unvisited.get)
+    	path_to_home = nx.shortest_path(mst, source=most_probable_vertex, target=home)
+    	remote_result = client.remote(most_probable_vertex, path_to_home[1])
+    	if remote_result > 0:
+    		bots_found += remote_result - bots_at[most_probable_vertex]
+    		if path_to_home[1] not in bots_locations:
+    			bots_locations.append(path_to_home[1])
+    		bots_at[path_to_home[1]] += remote_result
+    		
+
 
 
 def count_score(vertex, scout_result, lies):
